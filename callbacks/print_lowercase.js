6b1b1800a4131a8f17d55500ed9596cd5d05a5c2
Dilan <dilannebioglu@gmail.com>
2017-10-09 16:42:56 +0000

    printed uppercase

---
+++ callbacks/print_lowercase.js
@@ -0,0 +1,11 @@
+var getHTML = require('../http-functions');
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step.html'
+};
+function  (html) {
+  console.log(html);
+};
+
+getHTML(requestOptions, );
