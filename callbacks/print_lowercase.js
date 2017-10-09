9f18531674b99821a07afac61f4a6f43c2503b71
Dilan <dilannebioglu@gmail.com>
2017-10-09 16:45:37 +0000

    printed as lowercase

--- callbacks/print_lowercase.js
+++ callbacks/print_lowercase.js
@@ -2,10 +2,11 @@ var getHTML = require('../http-functions');
 
 var requestOptions = {
   host: 'sytantris.github.io',
-  path: '/http-examples/step.html'
+  path: '/http-examples/step6/lowercase.html'
 };
-function  (html) {
+function printLowerCase (html) {
+  html.toLowerCase();
   console.log(html);
 };
 
-getHTML(requestOptions, );
+getHTML(requestOptions, printLowerCase);
--- callbacks/print_reverse.js
+++ callbacks/print_reverse.js
@@ -2,7 +2,7 @@ var getHTML = require('../http-functions');
 
 var requestOptions = {
   host: 'sytantris.github.io',
-  path: '/http-examples/step.html'
+  path: '/http-examples/step6/reverse.html'
 };
 function  (html) {
   console.log(html);
