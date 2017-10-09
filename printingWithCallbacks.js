b37487b2aa7e0f0ad964d23e9658e0960a0025ed
Dilan <dilannebioglu@gmail.com>
2017-10-09 16:10:21 +0000

    Fixed some errors

--- printingWithCallbacks.js
+++ printingWithCallbacks.js
@@ -1,20 +1,20 @@
 var https = require('https');
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
+function printHTML (html) {
+  console.log(html);
+};
 function getHTML(options, callback) {
   var finalData = '';
-  function printHTML (html) {
-    console.log(html);
-  };
-  var requestOptions = {
-    host: 'sytantris.github.io',
-    path: '/http-examples/step4.html'
-  };
   https.get(requestOptions, function(response) {
     response.setEncoding('utf8');
     response.on('data', function(data) {
       finalData += data;
-      printHTML(finalData);
+      callback(finalData);
     });
   });
 }
 
-getHTML();
+getHTML(requestOptions, printHTML);
